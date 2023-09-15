c2d34c610b837cc9511d4c48f2df847f87016ab9
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-09-15 08:55:41 +0000

    codigo

--- Lanzador.java
+++ Lanzador.java
@@ -1,2 +1,33 @@
 //controlará el flujo de la aplicación, lanzando un proceso ProcesadorContabilidad para cada archivo y luego agregando todos los resultados
 
+public class Lanzador {
+    public static void main(String[] args) {
+        String[] archivos = { "informatica.txt", "gerencia.txt", "contabilidad.txt", "comercio.txt", "recursos_humanos.txt" };
+
+        // Crear hilos para procesar cada archivo
+        Thread[] threads = new Thread[archivos.length];
+        for (int i = 0; i < archivos.length; i++) {
+            threads[i] = new Thread(new ProcesadorContabilidad(archivos[i]));
+            threads[i].start();
+        }
+
+        // Esperar a que todos los hilos terminen
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Sumar todas las sumas de departamentos
+        long sumaGlobal = UtilidadesFicheros.obtenerSumaTransacciones(archivos);
+
+        // Guardar resultado global
+        try (PrintWriter pw = new PrintWriter(new FileWriter("Resultado_global.txt"))) {
+            pw.println(sumaGlobal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- UtilidadesFicheros.java
+++ UtilidadesFicheros.java
@@ -1,2 +1,21 @@
-//métodos útiles para el procesamiento de archivos, incluyendo uno para obtener la suma de las transacciones de un conjunto de archivos.
+import java.io.*;
+
+public class UtilidadesFicheros {
+    public static long obtenerSumaTransacciones(String[] archivos) {
+        long sumaTotal = 0;
+        for (String archivo : archivos) {
+            try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    long transaccion = Long.parseLong(linea);
+                    sumaTotal += transaccion;
+                }
+            } catch (IOException | NumberFormatException e) {
+                // Manejar excepciones de lectura y formato aquí
+                e.printStackTrace();
+            }
+        }
+        return sumaTotal;
+    }
+}
 
